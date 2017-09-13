509e4ea38d64446fb49352634206881663b2b65b
Piotr Mitros <piotr@mitros.org>
2017-09-13 13:39:26 -0400

    Scripts

---
+++ download_files.sh
@@ -0,0 +1,28 @@
+# Unifi
+
+# Default IP: 192.168.1.20
+# Default subnet: 255.255.255.0
+
+wget http://dl.ubnt.com/unifi/5.5.20/unifi_sysvinit_all.deb
+wget https://dl.ubnt.com/guides/UniFi/UniFi_AP-AC-M_QSG.pdf
+wget https://dl.ubnt.com/datasheets/unifi/UniFi_AC_Mesh_DS.pdf
+wget https://dl.ubnt.com/guides/UniFi/UniFi_Controller_V5_UG.pdf
+wget http://dl.ubnt.com/unifi/5.5.20/UniFi-installer.exe
+wget http://dl.ubnt.com/unifi/5.5.20/UniFi.pkg
+
+# Intel NUC BIOS Update
+
+wget https://downloadmirror.intel.com/25913/eng/NUC-BIOS-Update-Readme.pdf
+wget https://downloadmirror.intel.com/25913/eng/SY_0042_ReleaseNotes.pdf
+wget https://downloadmirror.intel.com/25913/eng/SY0042.BIO
+wget https://downloadmirror.intel.com/25913/eng/SYSKLI35.86A.0042.BI.ZIP
+wget https://downloadmirror.intel.com/25913/eng/SYSKLi35.86A.0042.EB.EXE
+wget https://downloadmirror.intel.com/25913/eng/SYSKLi35.86A.0042.EB.WINPE64.EXE
+
+# Intel NUC Manual
+
+wget https://www.intel.com/content/dam/support/us/en/documents/boardsandkits/nuc-kits/NUC6i3SYH_NUC6i5SYH_UserGuide03.pdf
+
+# Intel integrator toolkit
+
+wget https://downloadmirror.intel.com/26307/eng/Intel_Integrator_Toolkit_6.1.6.zip
---
+++ unifi.sh
@@ -0,0 +1,3 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
+sudo apt-get update
+sudo apt-get install unifi
